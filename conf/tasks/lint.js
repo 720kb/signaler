0b1e2f90600be27b1d87d17359fb81642b4c4980
wouldgo <wouldgo84@gmail.com>
2016-05-22 17:38:35 +0200

    removed jshint and jscs

--- conf/tasks/lint.js
+++ conf/tasks/lint.js
@@ -3,18 +3,13 @@
   'use strict';
 
   const gulp = require('gulp')
-      , jshint = require('gulp-jshint')
       , eslint = require('gulp-eslint')
-      , jscs = require('gulp-jscs')
-      , stylish = require('jshint-stylish')
       , path = require('path')
       , paths = require('../paths')
       , toLint = path.resolve(__dirname, '../..', paths.lib, '**/*.js')
       , gulpFolder = path.resolve(__dirname, '**/*.js');
 
-  gulp.task('lint', ['eslint', 'jshint', 'jscs']);
-
-  gulp.task('eslint', () => {
+  gulp.task('lint', () => {
 
     return gulp.src([gulpFolder, toLint])
       .pipe(eslint())
@@ -22,18 +17,4 @@
       .pipe(eslint.failOnError());
   });
 
-  gulp.task('jshint', () => {
-
-    return gulp.src([gulpFolder, toLint])
-      .pipe(jshint())
-      .pipe(jshint.reporter(stylish));
-  });
-
-  gulp.task('jscs', () => {
-
-    return gulp.src([gulpFolder, toLint])
-      .pipe(jscs())
-      .pipe(jscs.reporter())
-      .pipe(jscs.reporter('fail'));
-  });
 }());
